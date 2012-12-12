a8d53f9535c37c52c7c0c173623ad835715968fd
winbrous <andrei.broussillon@esigetel.fr>
2012-12-12 14:59:17 +0100

    rebuilding finish

---
+++ src/metier/MonEntity.java
@@ -0,0 +1,24 @@
+package metier;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class MonEntity {
+
+	private long id;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+	
+	
+}
