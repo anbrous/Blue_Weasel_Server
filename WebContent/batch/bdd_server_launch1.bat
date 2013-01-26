402862765c093ea7d538c356d5e631fd28f2067f
winbrous <andrei.broussillon@esigetel.fr>
2013-01-26 04:27:20 +0100

    batch pour les 2 machines esigetel

---
+++ WebContent/batch/bdd_server_launch1.bat
@@ -0,0 +1,14 @@
+echo off
+cls
+echo Bonjour, ICI Android
+echo  vous aller lancer le serveur de base de donnees
+pause
+cd E:/Users/esigetel/workspace/Blue_Weasel_Server/WebContent/WEB-INF/lib
+start java -classpath com.springsource.org.hsqldb-1.8.0.9.jar org.hsqldb.Server
+echo Le serveur est lance
+echo lancer l'interface de commande SQL, appuyer sur une touche pour lancer
+pause
+start java -classpath com.springsource.org.hsqldb-1.8.0.9.jar org.hsqldb.util.DatabaseManagerSwing
+cls
+echo Aurevoir !
+pause
